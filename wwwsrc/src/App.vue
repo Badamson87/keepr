0cbbcc550a985073034b776f674cc86df9fee5e1
brian <brianadamson87@gmail.com>
2019-01-19 17:26:45 -0700

    start layout

--- wwwsrc/src/App.vue
+++ wwwsrc/src/App.vue
@@ -1,5 +1,60 @@
 <template>
-  <div id="app">
+  <div id="app" >
+    
+
+ 
+
+
+  <!-- navbar -->
+  
+  
+  <div>
+    
+    <nav class="navbar navbar-expand-lg navbar-light bg-warning">
+      <a class="navbar-brand" href="#">Keepr</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Pricing</a>
+          </li>
+        </ul>
+        <span class="navbar-text">
+          <p>You Keep What You Take</p>
+        </span>
+      </div>
+    </nav>
+    
+
+    <img class="bkImg" src="assets/img/red.jpg">
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     <router-view />
   </div>
 </template>
@@ -24,5 +79,12 @@
 
   #nav a.router-link-exact-active {
     color: #42b983;
+  }
+
+  .bkImg{
+    position: fixed;
+    height: 100vh;
+   
+    
   }
 </style>
